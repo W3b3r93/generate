0a4c5965222083e541e22dae4893377fe19dfdfa
Weber <jorisweber1@gmail.com>
2025-12-30 20:43:16 +0100

    x

--- ww.sql
+++
@@ -1,7 +0,0 @@
-USE TheSixthString;
-
--- Welkom01! hash voor ALLE klanten
-UPDATE Klant SET wachtwoord_hash = '$2y$10$8G0gK1y7z9vW3xQ4rT5uP.6mN7oP8qR9sT0uV1wX2yZ3aB4cD5eF6' WHERE email LIKE '%windesheim.nl';
-
--- Check
-SELECT email, LEFT(wachtwoord_hash, 30) as hash_preview, account_status FROM Klant;
